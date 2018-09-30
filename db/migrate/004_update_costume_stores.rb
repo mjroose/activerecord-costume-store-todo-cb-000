182064d369dde8fbaf5e5525ebd7ff6f947ad69f
Michael Roose <mjroose@gmail.com>
2018-09-30 17:32:14 +0000

    automatically backed up by learn

---
+++ db/migrate/004_update_costume_stores.rb
@@ -0,0 +1,11 @@
+class UpdateCostumeStores < ActiveRecord::Migration[5.1]
+  def change
+    change_table :costume_stores do |t|
+      t.rename :employees_count, :num_of_employees
+      t.rename :in_business, :still_in_business
+    end
+
+    change_column :costume_stores, :opening_time, :datetime
+    change_column :costume_stores, :closing_time, :datetime
+  end
+end
