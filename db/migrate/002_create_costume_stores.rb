9c0239b32d472038bb336b7d6696223d46595711
Michael Roose <mjroose@gmail.com>
2018-09-30 17:35:55 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees_count
+      t.boolean :in_business
+      t.string :opening_time
+      t.string :closing_time
+    end
+  end
+end
---
+++ db/migrate/005_fix_haunted_houses.rb
@@ -0,0 +1,5 @@
+class FixHauntedHouses < ActiveRecord::Migration[5.1]
+  def change
+    rename_column :haunted_houses, :long_description, :description
+  end
+end
